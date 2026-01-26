65489dd575222fe718bc58dae88862ece30944bb
Ilya Livshits <jarto666@gmail.com>
2026-01-26 16:16:31 +0100

    f

--- apps/api/docker-entrypoint.sh
+++ apps/api/docker-entrypoint.sh
@@ -20,7 +20,7 @@ case "$SERVICE" in
     ;;
   migrations|migrate)
     echo "Running database migrations..."
-    exec npx prisma migrate deploy --schema=apps/api/prisma/schema.prisma
+    exec npx prisma migrate deploy --schema=/app/apps/api/prisma/schema.prisma
     ;;
   *)
     # Allow running arbitrary commands for debugging/one-off tasks
